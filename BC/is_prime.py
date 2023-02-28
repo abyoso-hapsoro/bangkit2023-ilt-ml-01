e05821aae314470da62d80bf5b91c1d0a5be24bc
hufahamdika <hufahamdika7997@gmail.com>
2023-02-28 14:37:16 +0000

    Revise that 1 is not a prime number and add case if input is 0 or negative

--- BC/is_prime.py
+++ BC/is_prime.py
@@ -1,23 +1,30 @@
 def is_prime(x):
   # initialize variable
-  num = x
-  flag = 0
+    num = x
+    flag = 0
+
+    if num < 2:
+        # special case, 1 is not a prime number
+        if num == 1:
+            return_string = "{} is not a prime number".format(num)
+
+        # return invalid if input is 0 or negative number
+        else : 
+            return_string = "Input is 0 or negative number"
+
+        return return_string
   
-  # 1 and 2 is a prime number
-  if num == 1 or num == 2:
-    return_string = "{} is a prime number".format(num)
-  
-  else:
-    # checking if number is divisible
-    for i in range(2, int((num + 1) / 2)):
-      # if it's divisible then it's not a prime number
-      if num % i == 0:
-        return_string = "{} is not a prime number".format(num)
-        flag = 1
-        break
+    # checking if number is divisible 
+    for i in range(2, int(num / 2) + 1):
+    
+        # if it's divisible then it's not a prime number
+        if num % i == 0:
+            return_string = "{} is not a prime number".format(num)
+            flag = 1
+            break
     
     # if it's not divisible then it's a prime number
     if flag == 0:
-      return_string = "{} is a prime number".format(num)
+        return_string = "{} is a prime number".format(num)
   
-  return return_string
+    return return_string
